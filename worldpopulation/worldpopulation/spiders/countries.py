4063ac5d5bd923235f30989417ebdf3746207819
Yash Mishra <yash01012002@gmail.com>
2023-06-21 09:27:23 +0530

    countries

--- worldpopulation/worldpopulation/spiders/countries.py
+++ worldpopulation/worldpopulation/spiders/countries.py
@@ -1,4 +1,5 @@
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -12,7 +13,15 @@ class CountriesSpider(scrapy.Spider):
             name = country.xpath("//text()").get()
             link = country.xpath(".//@href").get()
 
-        yield {
-            'country_name': name,
-            'country_link': link 
-        } 
+        # yield {
+        #     'country_name': name,
+        #     'country_link': link 
+        # } 
+
+        yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
+
+    def parse_country(self, response):
+        name = response.request.meta['country_name']
+        rows = response.xpath("(//table[@class='table table-striped table-bordered dataTable no-footer'])[1]/tbody/tr") 
+        for row in rows:
+            population = row.xpath("")
